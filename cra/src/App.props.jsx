603ed6c186e4e9e25c425d811f88fb366cf796a1
강찬석 <ssimjunlke@naver.com>
2021-09-23 14:32:04 +0900

    parent children

---
+++ cra/src/App.props.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropClass from './components/PropClass';
+import PropFunc from "./components/PropFunc";
+function App() {
+    const messageFromChild = (message) => {
+        alert(message);
+    }
+
+    return (
+        <div>
+            <h2>Props in Class Component</h2>
+            <PropClass prop_str="string value"
+                prop_num={ 10 }
+                prop_false={ false }
+                prop_true
+                prop_obj={{
+                    name: "강찬석",
+                    age: 27
+                }} 
+                prop_func={ messageFromChild }>
+                <h4>Children</h4>
+                <p>자식 컴포넌트</p>
+                </PropClass>
+                <h2>Props in Function Component</h2>
+                <PropFunc
+                prop_str = " string value"
+                prop_num = {10}
+                prop_false = {false}
+                prop_true
+                prop_obj ={{
+                    name: "강찬석",
+                    age : 27
+                }} 
+                prop_func = { messageFromChild }
+                />
+        </div>
+    )
+}
+
+export default App;
